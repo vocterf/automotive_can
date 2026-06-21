7f02f783d1883b8afcf1e59a68b9483f5e2eba5e
vocterf <vocterr07@gmail.com>
2026-06-21 18:50:14 +0200

    feat(main): implement zero-allocation network demultiplexer for multi-id decoding

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use automotive_can::can_matrix::{AbsWheelSpeeds, CanFrame};
+use automotive_can::can_matrix::{AbsWheelSpeeds, CanFrame, EngineData};
 use socketcan::{CanSocket, EmbeddedFrame, Frame, Socket};
 use std::error::Error;
 
@@ -11,11 +11,10 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         if frame.is_data_frame() {
             let id = frame.raw_id();
+            let raw_payload = frame.data();
 
-            if id == AbsWheelSpeeds::ID {
-                let raw_payload = frame.data();
-
-                match AbsWheelSpeeds::from_bytes(raw_payload) {
+            match id {
+                AbsWheelSpeeds::ID => match AbsWheelSpeeds::from_bytes(raw_payload) {
                     Ok(wheel_speeds) => {
                         println!(
                             "[ID: 0x{:X}] FL: {:.2} km/h | FR: {:.2} km/h | RL: {:.2} km/h | RR: {:.2} km/h",
@@ -32,7 +31,26 @@ fn main() -> Result<(), Box<dyn Error>> {
                             id, e
                         );
                     }
-                }
+                },
+                EngineData::ID => match EngineData::from_bytes(raw_payload) {
+                    Ok(engine_data) => {
+                        println!(
+                            "[ID: 0x{:X}] RPM: {} | PEDAL: {}",
+                            id,
+                            engine_data.rpm(),
+                            engine_data.pedal_position()
+                        )
+                    }
+
+                    Err(e) => {
+                        eprintln!(
+                            "[DIAG ERROR]: Malformed payload for ID: 0x{:X}: {:?}",
+                            id, e
+                        )
+                    }
+                },
+
+                _ => {}
             }
         }
     }
